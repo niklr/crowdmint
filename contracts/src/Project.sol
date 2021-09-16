290d23b176573891ec1b419ec8e0c7f373aff08b
blake256 <kultiviert@gmx.net>
2021-09-16 15:05:47 +0200

    contracts: add @openzeppelin/contracts dependency

---
+++ contracts/src/Project.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract Project {
+    using SafeMath for uint256;
+
+    struct ProjectInfo {
+        string title;
+        string category;
+        string url;
+        uint256 goal;
+        uint256 deadline;
+        address creator;
+    }
+
+    address public manager;
+    uint256 public totalContributions;
+    uint256 public totalContributors;
+    uint256 public totalFunding;
+
+    ProjectInfo public info;
+
+    modifier onlyManager() {
+        require(manager == msg.sender, "Only manager.");
+        _;
+    }
+
+    constructor(
+        string memory _category,
+        string memory _title,
+        string memory _url,
+        uint256 _goal,
+        uint256 _deadline,
+        address _creator
+    ) {
+        require(_goal > 0, "Funding goal must be greater than 0.");
+        require(_deadline > block.timestamp, "Deadline must be greater than current timestamp.");
+        require(_creator != address(0), "Creator address must be valid.");
+
+        manager = msg.sender;
+
+        info = ProjectInfo({
+            category: _category,
+            title: _title,
+            url: _url,
+            goal: _goal,
+            deadline: _deadline,
+            creator: _creator
+        });
+
+        totalContributions = 0;
+        totalContributors = 0;
+        totalFunding = 0;
+    }
+
+    function getInfo()
+        public
+        view
+        returns (
+            string memory, // category
+            string memory, // title
+            string memory, // url
+            uint256, // goal
+            uint256, // deadline
+            address, // creator
+            uint256, // totalContributions
+            uint256, // totalContributors
+            uint256, // totalFunding
+            address // manager
+        )
+    {
+        return (
+            info.category,
+            info.title,
+            info.url,
+            info.goal,
+            info.deadline,
+            info.creator,
+            totalContributions,
+            totalContributors,
+            totalFunding,
+            manager
+        );
+    }
+
+    /**
+     * Prevent ETH from being sent to this contract
+     */
+    fallback() external payable {
+        revert();
+    }
+
+    receive() external payable {
+        revert();
+    }
+}
--- contracts/src/ProjectManager.sol
+++ contracts/src/ProjectManager.sol
@@ -2,15 +2,48 @@
 
 pragma solidity ^0.7.0;
 
-contract ProjectManager {
-    address public owner;
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./Project.sol";
 
-    constructor() payable {
+contract ProjectManager {
+    using SafeMath for uint256;
+
+    address public owner;
+    uint256 public totalProjects;
+
+    mapping(uint256 => address) public projects;
+
+    event ProjectCreated(uint id, string category, string title, address addr, address creator);
+
+    constructor() {
         owner = msg.sender;
+        totalProjects = 0;
     }
 
     function getTimestamp() public view returns (uint256) {
         return block.timestamp;
+    }
+
+    function create(
+        string memory _category,
+        string memory _title,
+        string memory _url,
+        uint256 _goal,
+        uint256 _deadline
+    ) public returns (address) {
+        require(_goal > 0, "Funding goal must be greater than 0.");
+        require(_deadline > block.timestamp, "Deadline must be greater than current timestamp.");
+
+        Project project = new Project(_category, _title, _url, _goal, _deadline, msg.sender);
+
+        address addr = address(project);
+        projects[totalProjects] = addr;
+
+        emit ProjectCreated(totalProjects, _category, _title, addr, msg.sender);
+
+        totalProjects++;
+
+        return addr;
     }
 
     /**
